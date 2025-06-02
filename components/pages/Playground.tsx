97e989b32770596a25013e8116c18c5c72165019
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-06-03 01:30:29 +0530

    update: added role based filtering for user hooks

--- components/pages/Playground.tsx
+++ components/pages/Playground.tsx
@@ -1,9 +1,33 @@
+'use client'
+
+import useUsers from '@/hooks/useUsers'
 import React from 'react'
 
 const Playground = () => {
+  const { users, error, isLoading } = useUsers('', 'manager');
+  
   return (
-    <div>Playground</div>
+    <div>
+      {isLoading && <p>Loading users...</p>}
+      
+      {error && <p className="error">Error: {error.message}</p>}
+      
+      {users && (
+        <div className="users-container">
+          <h2>Users List</h2>
+          {users.length === 0 ? (
+            <p>No users found</p>
+          ) : (
+            <ul>
+              {users.map(user => (
+                <li>{user.displayName}, {user.role}, {user.location}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      )}
+    </div>
   )
 }
 
-export default Playground
+export default Playground;
