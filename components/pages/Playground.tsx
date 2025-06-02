37990bab25c1ca2b2e8b61988e147b369c7113fa
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-06-03 01:06:08 +0530

    trial: accountant transfer money

--- components/pages/Playground.tsx
+++ components/pages/Playground.tsx
@@ -1,33 +1,9 @@
-'use client'
-
-import useUsers from '@/hooks/useUsers'
 import React from 'react'
 
 const Playground = () => {
-  const { users, error, isLoading } = useUsers();
-  
   return (
-    <div>
-      {isLoading && <p>Loading users...</p>}
-      
-      {error && <p className="error">Error: {error.message}</p>}
-      
-      {users && (
-        <div className="users-container">
-          <h2>Users List</h2>
-          {users.length === 0 ? (
-            <p>No users found</p>
-          ) : (
-            <ul>
-              {users.map(user => (
-                <li>{user.displayName}, {user.role}, {user.location}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-      )}
-    </div>
+    <div>Playground</div>
   )
 }
 
-export default Playground;
+export default Playground
